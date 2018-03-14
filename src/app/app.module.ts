cb4b796db7ed961144744cdc72424cfdb3c383a5
Jeny <jenyspa@gmail.com>
2018-03-13 23:24:18 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPessoaComponent
   ],
   imports: [
     BrowserModule
